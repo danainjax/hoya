a41858942e7c7e6122c4aaa241dd52fb1217fb7d
danathedev <danainjax@gmail.com>
2023-10-18 15:27:03 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hoya</h1>
+      <h4> Day 1</h4>
+      <img src='hoya_1.jpeg' alt='Hoya Day 1' width='500' height='600' />
     </div>
   );
 }

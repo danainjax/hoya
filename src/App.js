c1652897caf05f142725d0438fc3d5e1400a2aa1
danathedev <danainjax@gmail.com>
2023-10-18 16:06:57 -0400

    delete test file and apply style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='App-header'>
       <h1>Hoya</h1>
       <h4> Day 1</h4>
       <img src='hoya_1.jpeg' alt='Hoya Day 1' width='500' height='600' />

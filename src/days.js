ec6aba6f957ddeeabd5c5b05648303e7431d2195
danathedev <danainjax@gmail.com>
2023-10-22 12:29:42 -0400

    day 5

--- src/days.js
+++ src/days.js
@@ -21,6 +21,11 @@ export const days = [
     'blogText': `10/20/2023 - TGIFriYAY : Beautiful day in Northern Florida. And, it's payday!`,
   },
   {
+    'title': 'Day 4',
+    'image': 'hoya_4.jpg',
+    'blogText': `10/21/2023 - Jax Symphony tonight. No change on the plant.`,
+  },
+  {
     'title': 'Coming Soon...',
     'image':
       'https://images.unsplash.com/photo-1636654129379-e7ae6f30bfd0?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29taW5nJTIwc29vbnxlbnwwfHwwfHx8MA%3D%3D',
@@ -28,8 +33,8 @@ export const days = [
   },
 
   {
-    'title': 'Day 4',
-    'image': 'hoya_4.jpg',
-    'blogText': `10/21/2023 - Jax Symphony tonight. No change on the plant.`,
+    'title': 'Day 5',
+    'image': 'hoya_5.jpg',
+    'blogText': `10/22/2023 - Simone Dinnerstein completely rocked Jacoby Hall last night. Amazing. New brain hamster wheel soundtrack is Philip Glass Etude 6`,
   },
 ];
